a2c094bd8d9b5e75a3aceb86aff9be11976fd9e6
manojgoud11111 <manojgoud111112gmail.com>
2022-08-09 17:46:15 +0530

    srikanth 1

---
+++ src/main/java/com/sr/srikanth.java
@@ -0,0 +1,13 @@
+package com.sr;
+
+/**
+ * Hello world!
+ *
+ */
+public class srikanth
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
